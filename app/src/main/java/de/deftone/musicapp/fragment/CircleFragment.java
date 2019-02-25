cf0553536a4237711f5db429fd58061550eb5c0d
Katrin <webentwicklerinKatrin@gmail.com>
2019-02-25 21:54:58 +0100

    semitransparente buttons auf quintenzirkel bild

--- app/src/main/java/de/deftone/musicapp/fragment/CircleFragment.java
+++ app/src/main/java/de/deftone/musicapp/fragment/CircleFragment.java
@@ -3,10 +3,14 @@ package de.deftone.musicapp.fragment;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import de.deftone.musicapp.R;
@@ -14,10 +18,34 @@ import de.deftone.musicapp.model.KeyData;
 
 public class CircleFragment extends Fragment {
 
+    @BindView(R.id.button_c)
+    Button buttonC;
+    @BindView(R.id.button_g)
+    Button buttonG;
+    @BindView(R.id.button_d)
+    Button buttonD;
+    @BindView(R.id.button_a)
+    Button buttonA;
+    @BindView(R.id.button_e)
+    Button buttonE;
+    @BindView(R.id.button_b)
+    Button buttonB;
+    @BindView(R.id.button_fis)
+    Button buttonFis;
+    @BindView(R.id.button_db)
+    Button buttonDb;
+    @BindView(R.id.button_ab)
+    Button buttonAb;
+    @BindView(R.id.button_eb)
+    Button buttonEb;
+    @BindView(R.id.button_bb)
+    Button buttonBb;
+    @BindView(R.id.button_f)
+    Button buttonF;
+
     public CircleFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,7 +53,73 @@ public class CircleFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_circle, container, false);
         ButterKnife.bind(this, view);
+        initButtons();
         return view;
+    }
+
+    private void initButtons() {
+        DisplayMetrics metrics = new DisplayMetrics();
+
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metrics);
+        int usableHeight = metrics.heightPixels;
+        int usableWidth = metrics.widthPixels;
+
+        //real height is larger and contains also status bar and soft keys bar (i guess)
+//        getActivity().getWindowManager().getDefaultDisplay().getRealMetrics(metrics);
+//        int realHeight = metrics.heightPixels; //width is the same!
+
+        int actionbarHeight = ((AppCompatActivity) getActivity()).getSupportActionBar().getHeight();
+
+        //size of button in px (although it is defined in dp)
+        int buttonWidth = (int) getResources().getDimension(R.dimen.button_width);
+
+        //center at: (and also move button horizontally)
+        int centerX = usableWidth / 2 - buttonWidth / 2;
+        int centerY = usableHeight / 2 - actionbarHeight - buttonWidth / 5;
+
+        float sin30 = (float) Math.sin(30. / 180 * Math.PI);
+        float cos30 = (float) Math.cos(30. / 180 * Math.PI);
+
+        float distanceFromCenter = .85f * centerX;
+
+        //the 4 buttons "on the cross" (i.e 0, 3, 6 and 9 o'clock)
+        buttonC.setX(centerX);
+        buttonC.setY(centerY - distanceFromCenter);
+
+        buttonA.setX(centerX + distanceFromCenter);
+        buttonA.setY(centerY);
+
+        buttonEb.setX(centerX - distanceFromCenter);
+        buttonEb.setY(centerY);
+
+        buttonFis.setX(centerX);
+        buttonFis.setY(centerY + distanceFromCenter);
+
+        //the 4 buttons shifted by 30 degrees from y-axis (i.e. 1, 5, 7 and 11 o'clock)
+        buttonG.setX(centerX + sin30 * distanceFromCenter);
+        buttonG.setY(centerY - cos30 * distanceFromCenter);
+
+        buttonF.setX(centerX - sin30 * distanceFromCenter);
+        buttonF.setY(centerY - cos30 * distanceFromCenter);
+
+        buttonB.setX(centerX + sin30 * distanceFromCenter);
+        buttonB.setY(centerY + cos30 * distanceFromCenter);
+
+        buttonDb.setX(centerX - sin30 * distanceFromCenter);
+        buttonDb.setY(centerY + cos30 * distanceFromCenter);
+
+        // the 4 buttons shifted by 30 degrees from x-axis (i.e.  2, 4, 8 and 10 o'clock)
+        buttonD.setX(centerX + cos30 * distanceFromCenter);
+        buttonD.setY(centerY - sin30 * distanceFromCenter);
+
+        buttonE.setX(centerX + cos30 * distanceFromCenter);
+        buttonE.setY(centerY + sin30 * distanceFromCenter);
+
+        buttonBb.setX(centerX - cos30 * distanceFromCenter);
+        buttonBb.setY(centerY - sin30 * distanceFromCenter);
+
+        buttonAb.setX(centerX - cos30 * distanceFromCenter);
+        buttonAb.setY(centerY + sin30 * distanceFromCenter);
     }
 
     private void openScaleFragment(KeyData keyData) {
@@ -38,7 +132,6 @@ public class CircleFragment extends Fragment {
         ft.addToBackStack(null);
         ft.commit();
     }
-
 
     @OnClick(R.id.button_c)
     void showScaleC() {
@@ -99,5 +192,4 @@ public class CircleFragment extends Fragment {
     void showScaleDb() {
         openScaleFragment(KeyData.DB);
     }
-
 }
